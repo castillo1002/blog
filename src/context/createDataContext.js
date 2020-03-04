11583441cf92e8e390f8942d67110e2c4062c0f3
castillin <castillo@mitcon.org>
2020-03-04 07:13:32 -0500

    Abstracting Context

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -6,8 +6,19 @@ export default (reducerFunction, actions, initialState) => {
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducerFunction, initialState);
 
+    //actions === { addBlogPost: (dispatch) => { return () => {} } }
+    const boundActions = {};
+    for (let key in actions) {
+      // Line below: boundActions.addBlogPost = () => dispatch({ type: "add_blogpost" })
+      boundActions[key] = actions[key](dispatch);
+    }
+
     return (
-      <Context.Provider value={{ state: state }}>{children}</Context.Provider>
+      <Context.Provider value={{ state: state, ...boundActions }}>
+        {children}
+      </Context.Provider>
     );
   };
+
+  return { Context, Provider };
 };
